3769f4cba65fa94595c21cef39eb0cd3d4f662a7
Ciara O'Brien <ciaraobrienf@gmail.com>
2020-12-29 06:50:08 -0600

    Might work on some other stuff for the time being tbh, come back fresh

--- playground/src/main/scala/play/Playground.scala
+++ playground/src/main/scala/play/Playground.scala
@@ -10,6 +10,28 @@ import dottytags.styles._
 
 object Playground:
   def main(args: Array[String]): Unit =
-    Phased.phasedTest("test")
-    val test = "test"
-    Phased.phasedTest(test)
+    val numVisitors: Int = 1023
+    val posts: Seq[(String, String)] = Seq(
+      ("alice", "i like pie"),
+      ("bob", "pie is evil i hate you"),
+      ("charlie", "i like pie and pie is evil, i hat myself")
+    )
+    val bold: String = System.currentTimeMillis.toString
+
+    def calc(): String = html(
+      head(
+        script("some script")
+
+      ),
+      body(
+        h1("This is my <<<< title", cssPx("weight") := System.currentTimeMillis.toString),
+        div("posts"),
+        for ((name, text) <- posts) yield div(
+            h2("Post by ", name),
+            p(text)
+          ),
+        frag(raw("<"), ">"),
+        cls := "test",
+        if numVisitors > 100 then p("No more posts!")
+        else p("Please post below...")
+    )).render
