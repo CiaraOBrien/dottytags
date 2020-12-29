1abb2980b7b789040a9d6ba0356c1b8af4a1e085
Ciara O'Brien <ciaraobrienf@gmail.com>
2020-12-29 05:38:34 -0600

    Some more thoughts

--- shared/src/main/scala/dottytags/Phased.scala
+++ shared/src/main/scala/dottytags/Phased.scala
@@ -28,14 +28,16 @@ object Phased {
 
   def defer[A : Type : FromExpr : ToExpr](x: Expr[A])(using Quotes): Phased[A] = Dynamic(x)
 
-  // This needs a typeclass, PhaseOp, with Option[A => B], Expr[A] => Expr[B], and A => Phased[B],
+  // This needs a typeclass, PhaseOp, with Option[A => B] and Expr[A] => Expr[B],
   // That is: they may be able to apply the operation to a static value, they can defer the operation into a dynamic expression,
-  // and they can potentially "contaminate" the output, making it dynamic if it was static before, for operations which either are known to
-  // be dymamic or are just not known to be static. A PhaseOp can be generated for any method application,
-  // but unknown operations will have no A => B and will therefore dynamicize anything they're applied to.
+  // but if they lack Option[A => B] they "contaminate" the output, making it dynamic if it was static before, 
+  // this is for operations which either are known to be dymamic or are just not known to be static. 
+  // A PhaseOp can be generated for any method application, but unknown operations will have no A => B and will 
+  // therefore dynamicize anything they're applied to since they must be assumed to be dynamic.
 
   // It also needs a way to splice together a list of dynamic and static values.
   // Applicative-ish stuff should be good for that, through something like a traverse or sequence, idk yet.
+  // Could just build a list like with Splice.
 
   //def flatten[A : Type : FromExpr : ToExpr](p: Phased[Phased[A]])(using Quotes): Phased[A] = p match 
     //case Phased.Static (a:      Phased[A] ) => a
